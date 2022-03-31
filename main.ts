2ce63053c55da4d270241b358db0e07dabb18828
Laalmej <102734802+Laalmej@users.noreply.github.com>
2022-03-31 13:44:41 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -10,7 +10,11 @@ basic.forever(function () {
         `)
     if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 20)
-    } else if (false) {
-    	
+    }
+    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 20)
+    }
+    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 20)
     }
 })
